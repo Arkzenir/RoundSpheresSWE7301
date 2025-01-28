9620b62f699b6572fbaf8efa6abaa0681cdf0803
Tijani Saheed Babatomiwa <82809607+DonTeejay01@users.noreply.github.com>
2025-01-28 09:49:15 +0000

    Delete django directory

--- django/app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import ProductModel
-
-@admin.register(ProductModel)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'stock')
-    search_fields = ('name',)
-
-# Register your models here.
